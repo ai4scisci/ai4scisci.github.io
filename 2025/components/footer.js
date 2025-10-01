ff01a8e8f814f280787179604f0bce7cb0a8cccc
rochanaro <rochana.odu@gmail.com>
2025-09-30 23:15:38 -0400

    draft for year 2025 added

---
+++ 2025/components/footer.js
@@ -0,0 +1,78 @@
+class Footer2025 extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const startYear = 2023;
+    const currentYear = new Date().getFullYear();
+    const yearDisplay = startYear === currentYear ? `${startYear}` : `${startYear}–${currentYear}`;
+    this.innerHTML = `
+    <div class="footer-lemos mt-4 px-3">
+    <div class="pt-2 pb-3 container">
+  
+ 
+      <!-- Contacts row -->
+      <div class="row mt-3 px-md-3">
+        <div class="col-12 col-md-6">
+          <h4 class="mb-1 fw-bold">Contact</h2>
+          <p class="mb-0"><b>Jian Wu, Ph.D.</b><br>
+            Associate Professor<br>
+            Department of Computer Science<br>
+            Old Dominion University<br>
+            3202 ECS Building, Norfolk, VA, 23529<br>
+            Tel: 757-683-7753<br>
+            E-mail: <a href="mailto:jwu@cs.odu.edu">jwu@cs.odu.edu</a><br>
+          </p>
+        </div>
+
+      </div>
+  
+      <!-- Email/social row (existing) -->
+      <div class="row mt-3">
+        <div class="col-auto col-6 align-middle pr-0">
+          <p class="my-0 pt-0"><a href="mailto:"></a></p>
+        </div>
+        <div class="col-auto col-6 text-right pl-0">
+          <!-- social icons or other links -->
+        </div>
+      </div>
+
+      <div class="row px-md-3">
+      <div class="col-auto col-md-5 align-middle">
+        <p class="mb-0">© ${yearDisplay} <b>AI4SciSci</b></p>
+      </div>
+    </div>
+  
+    </div>
+  </div>
+  
+    `;
+  }
+}
+
+customElements.define('footer-component-2025', Footer2025);
+
+
+{/* <h4 class="mb-1 fw-bold">Contact</h2>
+<p class="mb-0"><b>Jian Wu, Ph.D.</b><br>
+  Associate Professor<br>
+  Department of Computer Science<br>
+  Old Dominion University<br>
+  3202 ECS Building, Norfolk, VA, 23529<br>
+  Tel: 757-683-7753<br>
+  E-mail: <a href="mailto:jwu@cs.odu.edu">jwu@cs.odu.edu</a><br>
+</p> */}
+
+{/* <div class="col-12 col-md-6 mt-3 mt-md-0">
+<p class="mb-1 fw-bold">Contacts</p>
+<p class="mb-0">Yi He, Ph.D.<br>
+  Assistant Professor<br>
+  Department of Computer Science<br>
+  Old Dominion University<br>
+  3108 ECS Building, Norfolk, VA 23529<br>
+  Tel: 757-683-7821<br>
+  E-mail: <a href="mailto:yihe@cs.odu.edu">yihe@cs.odu.edu</a><br>
+  Webpage: <a href="#">Homepage</a>
+</p>
+</div> */}
---
+++ 2025/components/header.js
@@ -0,0 +1,64 @@
+class Header2025 extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+
+        <div class="container-fluid">
+        <a class="navbar-brand" href="/2025/">
+        <img src="/assets/img/AI4SciSci_v2.png" width="" height="50" class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#call-for-papers">Call for Papers</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#submissions">Submissions</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#key-dates">Key Dates</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#program">Program</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#keynote-speaker">Keynote Speaker</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#program-committee">Program Committee</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="#contacts">Contacts</a>
+            </li>
+
+
+          </ul>
+        </div>
+        </div>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define('header-component-2025', Header2025);
+
+{/* <li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+  Previous Conferences
+</a>
+<div class="dropdown-menu">
+  <a class="dropdown-item" href="/2023/">AI4SciSci 2023</a>
+</div>
+</li> */}
